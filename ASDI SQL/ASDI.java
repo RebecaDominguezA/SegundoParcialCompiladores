cbc3c37edb0533c6b88e3020ae93494934241fee
RebecaDominguezA <137738289+RebecaDominguezA@users.noreply.github.com>
2023-11-19 23:14:19 -0600

    parse ASDI.java
    agregue lo que se hace dentro del parse y la produccion de Q segun sus celdas celdas en la tabla

--- ASDI SQL/ASDI.java
+++ ASDI SQL/ASDI.java
@@ -1,5 +1,27 @@
 import java.util.List;
 
 public class ASDI implements Parser{
-  
+Pila miPila = new Pila();
+    private boolean hayErrores = false;
+    private String LINEA; //Aqui se guarda entrada
+
+    public ASDI(List<Token> tokens,String linea){
+        this.LINEA=linea;
+    }
+
+    @Override
+    public boolean parse() {
+        String primeraPalabra = ObtenerPrimerPalabra.obtener(LINEA);
+        Q(primeraPalabra);
+        //System.out.println(primeraPalabra+ "\n");
+        //preanalisis = this.tokens.get(i);
+        // = this.tokens.primeraPalabra;
+        if(!hayErrores){
+            System.out.println("Consulta correcta");
+            return  true;
+        }else {
+            System.out.println("Se encontraron errores");
+        }
+        return false;
+    }  
 }
