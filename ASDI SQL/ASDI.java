513e4e10f0cd603e69d4e35a7800e0fdb418c72f
Connor Urbano Mendoza <134821826+Connor-UM-18@users.noreply.github.com>
2023-11-19 23:18:45 -0600

    Update ASDI.java

--- ASDI SQL/ASDI.java
+++ ASDI SQL/ASDI.java
@@ -23,5 +23,28 @@ Pila miPila = new Pila();
             System.out.println("Se encontraron errores");
         }
         return false;
-    }  
+    }
+    // Q
+    private void Q(String palabra){
+        //Consumimos un elemento de la pila,
+        String Y=miPila.operacion(2,""); // Quitar un elemento (pop) y recibirlo
+        //System.out.println(palabra+ "and"+ Y+ ".");
+        //System.out.println(String.valueOf(TipoToken.SELECT)+ ".");
+        //SELECT x Q, nos da: Q -> select D from T
+        if((palabra.equals(String.valueOf(TipoToken.SELECT)))&&(Y.equals("Q"))){
+            //System.out.println("check");
+            // y agregamos su produccion corresponciente.
+            miPila.operacion(1,"T"); // Add un elemento (push)
+            miPila.operacion(1,"FROM"); // Add un elemento (push)
+            miPila.operacion(1,"D"); // Add un elemento (push)
+            miPila.operacion(1,"SELECT"); // Add un elemento (push)
+            
+            //Sabemos que palabra de entrada es SELECT y lo ultimo de pila es SELECT, match
+            match(palabra, "SELECT");
+        }else{
+            hayErrores = true;
+            System.out.println("Error sintactico.");
+            return;
+        }
+    }
 }
