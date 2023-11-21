4583730a4fd7b7266d7f06aa5259794b7ef1c7d7
Denisse M <105137096+dnssmarquez@users.noreply.github.com>
2023-11-20 20:46:48 -0600

    Update QuitarPrimerPalabra.java
    Esta clase nos ayudará a recibir una entrada de tipo string en la que existirán dos condiciones, la primera que es cuando existan más de dos palabras en donde lo que hará es quitar la primer palabra que exista en el string. Para la segunda condición si solo existe una palabra simplemente retornamos una cadena vacía.

--- ASDI SQL/QuitarPrimerPalabra.java
+++ ASDI SQL/QuitarPrimerPalabra.java
@@ -1,1 +1,12 @@
-
+public class QuitarPrimerPalabra {
+    public static String quitar(String cadena) {
+        int indiceEspacio = cadena.indexOf(" ");
+        
+        if (indiceEspacio != -1) {
+            return cadena.substring(indiceEspacio + 1);
+        } else {
+            // Si no se encuentra un espacio en blanco, retorna una cadena vacía o null, según sea necesario
+            return ""; 
+        }
+    }
+}
