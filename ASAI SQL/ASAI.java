4fd1f87a45af9850d95e50b02c5c4fe0dac6acac
RebecaDominguezA <137738289+RebecaDominguezA@users.noreply.github.com>
2023-11-26 16:25:37 -0600

    pila extra ASAI.java
    Se agrego una pila extra que funcionara para almacenar los acarreos de la pila 1, en la función parse mandamos a mandar al estado cero donde mandamos como parámetro la primer palabra de la entrada que se haya digitado.

--- ASAI SQL/ASAI.java
+++ ASAI SQL/ASAI.java
@@ -1,5 +1,30 @@
 import java.util.List;
 
 public class ASAI implements Parser{
+  Pila miPila = new Pila();
+    Pila2 Pila_Acarreo = new Pila2();
+    private boolean hayErrores = false;
+    private String LINEA; //Aqui se guarda entrada
+    private String Memoria="";
+
+    public ASAI(List<Token> tokens,String linea){
+        this.LINEA=linea;
+    }
+
+    @Override
+    public boolean parse() {
+        String primeraPalabra = ObtenerPrimerPalabra.obtener(LINEA);
+        E0(primeraPalabra);
+        //System.out.println(primeraPalabra+ "\n");
+        //preanalisis = this.tokens.get(i);
+        // = this.tokens.primeraPalabra;
+        if(!hayErrores){
+            System.out.println("Consulta correcta");
+            return  true;
+        }else {
+            System.out.println("Se encontraron errores");
+        }
+        return false;
+    }
   
 }
