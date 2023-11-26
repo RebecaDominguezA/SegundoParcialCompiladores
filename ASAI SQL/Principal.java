a059c519ebd953dc6a2b9a258e7df98cd6b28214
Connor Urbano Mendoza <134821826+Connor-UM-18@users.noreply.github.com>
2023-11-26 16:17:45 -0600

    Update Principal.java
    Solo se cambio 2 lineas de codigo con respecto al ASDI, solo se cambio la funcion a llamar, ahora llamamos a nuestro ASAI

--- ASAI SQL/Principal.java
+++ ASAI SQL/Principal.java
@@ -1,1 +1,60 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Principal {
+    
+    static boolean existenErrores = false;
+
+    public static void main(String[] args) throws IOException {
+        ejecutarPrompt();
+    }
+
+    private static void ejecutarPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("ASAI \n");
+            System.out.print(">>> ");
+            String linea = reader.readLine();
+            if(linea == null) break; // Presionar Ctrl + D
+            ejecutar(linea);
+            existenErrores = false;
+        }
+    }
+
+    private static void ejecutar(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        String ENTRADA="";
+        for(Token token : tokens){
+            //System.out.println(token);
+            ENTRADA =ENTRADA+(ObtenerPrimerPalabra.obtener(String.valueOf(token)));
+            ENTRADA =ENTRADA+" ";
+        }
+        ENTRADA=ENTRADA.substring(0, ENTRADA.length() - 1);//SIRVE PARA QUITAR Espacio del final
+        //System.out.println(ENTRADA);
+        Parser parser = new ASAI(tokens,ENTRADA);
+        parser.parse();
+    }
+
+    /*
+    El método error se puede usar desde las distintas clases
+    para reportar los errores:
+    Interprete.error(....);
+     */
+    static void error(int linea, String mensaje){
+        reportar(linea, "", mensaje);
+    }
+
+    private static void reportar(int linea, String donde, String mensaje){
+        System.err.println(
+                "[linea " + linea + "] Error " + donde + ": " + mensaje
+        );
+        existenErrores = true;
+    }
+
+}
