c2a138e9ca3fd2d59d85687d508477bb69041550
Denisse M <105137096+dnssmarquez@users.noreply.github.com>
2023-11-26 16:31:12 -0600

    Update ASAI.java
    Se agregó el estado0 (E0) y estado1 (E1) donde en el E1 y en el E2 se agregaron transiciones correspondientes según la tabla

--- ASAI SQL/ASAI.java
+++ ASAI SQL/ASAI.java
@@ -27,4 +27,49 @@ public class ASAI implements Parser{
         return false;
     }
   
+    // Estado E0
+    private void E0(String palabra){
+        //Consumimos un elemento de la pila,
+        String Y=miPila.operacion(2,""); // Quitar un elemento (pop) para verlo, guardarlo y lo volvemos a meter
+        //miPila.operacion(1, Y);//volvemos a meter
+        //System.out.println(palabra+ "and"+ Y+ ".");
+        //System.out.println(String.valueOf(TipoToken.SELECT)+ ".");
+        
+        //Q x 0, nos da: E0 -> E1
+        if((Memoria.equals("Q"))&&(Y.equals("0"))){
+            Memoria="";
+            miPila.operacion(1, "1");
+            quien_sigue("1");
+        }
+        //SELECT x 0, nos da: E0 -> S2
+        else if((palabra.equals(String.valueOf(TipoToken.SELECT)))&&(Y.equals("0"))){
+            //System.out.println("check");
+            // Se hace shift...
+            SHIFT("2",palabra);
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Error sintactico.");
+            return;
+        }
+    }
+    // Estado E1
+    private void E1(String palabra){
+        //Consumimos un elemento de la pila,
+        String Y=miPila.operacion(2,""); // Quitar un elemento (pop) y recibirlo
+        //System.out.println(palabra+ "Dand"+ Y+ ".");
+        //EOF x 1, nos da: 1-> ACCEPT
+        if((palabra.equals(String.valueOf(TipoToken.EOF)))&&(Y.equals("1"))){
+            // y agregamos su produccion corresponciente.
+            //En este caso solo ACEPTA y regresamos
+            return;
+        }
+        else{
+            hayErrores = true;
+            System.out.println("Error sintactico.");
+            return;
+        }
+    }
+  
 }
+
