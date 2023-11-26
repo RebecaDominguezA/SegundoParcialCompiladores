4799744026df40c759dde3c51bf779bb6d6fe226
Connor Urbano Mendoza <134821826+Connor-UM-18@users.noreply.github.com>
2023-11-26 16:15:43 -0600

    Update Pila.java
    solo se cambio una linea con respecto al anterior en la accion personalizada 2

--- ASAI SQL/Pila.java
+++ ASAI SQL/Pila.java
@@ -1,1 +1,48 @@
+import java.util.Stack;
+
+public class Pila {
+    private Stack<String> pila;
+
+    public Pila() {
+        //Se inicializa la pila
+        pila = new Stack<>();
+        pila.push("0");
+
+    }   
+
+    public String operacion(int accion, String Nuevo_elemento) {
+        switch (accion) {
+            case 0:
+                pop();
+                return "";
+            case 1:
+                push(Nuevo_elemento);
+                return "";
+            case 2:
+                // Realizar una acción personalizada
+                String elementoExtraido = pila.pop();
+                //System.out.println("Acción personalizada realizada");
+                pila.push(elementoExtraido);
+                return elementoExtraido;
+            default:
+                System.out.println("Acción no reconocida");
+                return "";
+        }
+    }
+
+    private void pop() {
+        if (!pila.isEmpty()) {
+            pila.pop();
+            //String elemento = pila.pop();
+            //System.out.println("Pop realizado. Valor extraído: " + elemento);
+        } else {
+            //System.out.println("La pila está vacía. No se puede hacer pop.");
+        }
+    }
+
+    private void push(String Nuevo_elemento) {
+        pila.push(Nuevo_elemento);
+        //System.out.println("Push realizado. Nuevo valor agregado: " + Nuevo_elemento);
+    }
+}
 
